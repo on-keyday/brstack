a1ed60a3aafedc3c69694589be2567bf6b8768a5
on-keyday <62627905+on-keyday@users.noreply.github.com>
2025-05-12 01:40:23 +0900

    add address definitions of MacAddress and Ipv4Address

--- protocol/common/src/lib.rs
+++ protocol/common/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct MacAddress(pub [u8; 6]);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub struct Ipv4Address(pub [u8; 4]);
