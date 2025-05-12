7c451951e53c7485f127f6ca5bce435b1c106d1a
on-keyday <62627905+on-keyday@users.noreply.github.com>
2025-05-12 14:16:25 +0900

    add socket2 and tokio dependency

--- protocol/ethernet/src/lib.rs
+++ protocol/ethernet/src/lib.rs
@@ -3,6 +3,7 @@ pub struct NetworkInterface {
     pub name: String,
     pub mac_address: net_common::MacAddress,
     pub ipv4_address: net_common::Ipv4Prefix,
+    pub socket :tokio::io::unix::AsyncFd<socket2::Socket>,
 }
 
 #[cfg(test)]
