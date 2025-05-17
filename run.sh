ea9464bfdcd6f4b158730d1834a8bb1e592f9d2b
on-keyday <62627905+on-keyday@users.noreply.github.com>
2025-05-18 03:32:17 +0900

    add tcpdump to observe garp

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 set -e # Exit on error
 nft -f /app/ruleset.txt 
 nft list ruleset 
-/app/brstack
+tcpdump -i eth0 -n -s 0 -w /app/pcap.pcap &
+/app/brstack &
 while true; do # for debugging
     sleep 1
 done
