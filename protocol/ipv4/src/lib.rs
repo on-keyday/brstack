237cdc3c03dc88015c8d692c3c4201c6e544b124
on-keyday <62627905+on-keyday@users.noreply.github.com>
2025-05-20 21:06:54 +0900

    implement RoutingTable

--- protocol/ipv4/src/lib.rs
+++ protocol/ipv4/src/lib.rs
@@ -1,14 +1,67 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+
+pub struct RoutingEntry {
+    pub prefix :net_common::Ipv4Prefix,
+    pub next_hop: net_common::Ipv4Address,
+    pub device : ethernet::NetworkInterface,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct RoutingTrieNode {
+    pub children: [Option<Box<RoutingTrieNode>>; 2],
+    pub entry: Option<std::sync::Arc<RoutingEntry>>,
+}
+impl RoutingTrieNode {
+    pub fn new() -> Self {
+        RoutingTrieNode {
+            children: [None, None],
+            entry: None,
+        }
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct RoutingTable {
+    root: tokio::sync::RwLock<RoutingTrieNode>,
+}
+
+impl RoutingTable {
+    pub fn new() -> Self {
+        RoutingTable {
+            root: tokio::sync::RwLock::new(RoutingTrieNode::new()),
+        }
+    }
+
+    pub async fn insert(&mut self, prefix: net_common::Ipv4Prefix, next_hop: net_common::Ipv4Address, device: ethernet::NetworkInterface) {
+        let mut node = self.root.write().await;
+        let mut node = &mut *node;
+        for i in (0..prefix.prefix_length).rev() {
+            let bit = (prefix.address.0[(i / 8) as usize] >> (7 - (i % 8))) & 1;
+            if node.children[bit as usize].is_none() {
+                node.children[bit as usize] = Some(Box::new(RoutingTrieNode::new()));
+            }
+            node = node.children[bit as usize].as_mut().unwrap();
+        }
+        node.entry = Some(std::sync::Arc::new(RoutingEntry {
+            prefix,
+            next_hop,
+            device,
+        }));
+    }
+
+    pub async fn lookup(&self, address: &net_common::Ipv4Address) -> Option<std::sync::Arc<RoutingEntry>> {
+        let node = self.root.read().await;
+        let mut node = &*node;
+        let mut best_entry: Option<std::sync::Arc<RoutingEntry>> = None;
+
+        for i in (0..32).rev() {
+            if let Some(entry) = &node.entry {
+                best_entry = Some(entry.clone());
+            }
+            let bit = (address.0[(i / 8) as usize] >> (7 - (i % 8))) & 1;
+            if node.children[bit as usize].is_none() {
+                break;
+            }
+            node = node.children[bit as usize].as_ref().unwrap();
+        }
+
+        best_entry
     }
 }
